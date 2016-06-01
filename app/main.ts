7dd81cdce63be5e529bf37d77acfe6d4074c21c2
Isabella Romare <isabella.romare@cinteros.se>
2016-06-01 13:31:51 +0200

    BAH

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Imports for loading & configuring the in-memory web api
 import { provide }    from '@angular/core';
 // The usual bootstrapping imports
@@ -10,13 +9,3 @@ import { AppComponent }   from './app.component';
 bootstrap(AppComponent, [
     HTTP_PROVIDERS     // in-mem server data
 ]);
-=======
-import { bootstrap }     from '@angular/platform-browser-dynamic';
-import { AppComponent }  from './app.component';
-
-import { HTTP_PROVIDERS } from '@angular/http';
-
-bootstrap( AppComponent, [
-    HTTP_PROVIDERS
-]);
->>>>>>> 9730b420246f7f3defe0a82d5f0f98b17f0b022c
